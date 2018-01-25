7f91bfae4805e0252528d868459473e0bed13f84
gaurav <gaurav@hp>
2018-01-25 15:43:03 +0530

    Spring mvc my first project

---
+++ spring mvc/src/com/sys/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.sys;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class WelcomeController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
+		
+		ModelAndView mv = new ModelAndView("hello");
+		mv.addObject("msg", "Welcome spring mvc");
+		
+		return mv;
+	}
+
+}
